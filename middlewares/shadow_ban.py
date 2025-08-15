9a901b9e1e8253f237564b047758cf178f4952e1
dystrophyless <romansokolov689@gmail.com>
2025-08-15 12:02:00 +0500

    Optimize logic so that we don't have to access database multiple times unnecessarily and add logs

--- middlewares/shadow_ban.py
+++ middlewares/shadow_ban.py
@@ -1,10 +1,9 @@
 import logging
 from typing import Any, Awaitable, Callable
-from psycopg import AsyncConnection
 
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Update, User
-from database.db import get_user_banned_status_by_id
+from aiogram.types import TelegramObject, User
+from aiogram.fsm.context import FSMContext
 
 
 logger = logging.getLogger(__name__)
@@ -14,26 +13,37 @@ class ShadowBanMiddleware(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: Update,
+        event: TelegramObject,
         data: dict[str, Any]
     ) -> Any:
+        logger.debug("Входим в ShadowBanMiddleware")
+
         user: User = data.get("event_from_user")
 
         if user is None:
+            logger.warning("По какой-то неизвестной причине пользователя не удалось определить, переходим в следующий \"обработчик\"")
             return await handler(event, data)
 
-        conn: AsyncConnection = data.get("conn")
+        state: FSMContext = data.get("state")
 
-        if conn is None:
-            logger.error("Соединение с базой данных не было найдено в данных мидлвари")
-            raise RuntimeError("Отстуствует соединение с базой данных для проверки теневого бана")
+        user_row = await state.get_value("user_row")
 
-        user_banned_status = await get_user_banned_status_by_id(conn, user_id=user.id)
+        if user_row is None:
+            logger.debug("Данные о пользователе не удалось получить из базы данных")
+            return await handler(event, data)
+
+        user_banned_status = user_row[7]
 
         if user_banned_status:
-            logger.warning("Забаненный пользователь с `user_id`='%d' попытался взаимодействовать с ботом", user.id)
+            logger.warning("Забаненный пользователь с `user_id`='%d' попытался взаимодействовать с ботом, игнорируем апдейт", user.id)
             if event.callback_query:
                 await event.callback_query.answer()
             return
 
-        return await handler(event, data)
+        logger.warning("Пользователь не в бане, переходим в следующий \"обработчик\"")
+
+        result = await handler(event, data)
+
+        logger.debug("Выходим из ShadowBanMiddleware")
+
+        return result
