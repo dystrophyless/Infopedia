8246c23dc3c70b44a08a2f7b8c7aefd4823ebcda
dystrophyless <romansokolov689@gmail.com>
2025-08-15 12:02:16 +0500

    Optimize logic so that we don't have to access database multiple times unnecessarily and add logs

--- middlewares/statistics.py
+++ middlewares/statistics.py
@@ -1,11 +1,13 @@
 import logging
 from typing import Any, Awaitable, Callable
+
+from aiogram.fsm.context import FSMContext
 from psycopg import AsyncConnection
 
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Update, User
+from aiogram.types import TelegramObject, User
 
-from database.db import get_user, add_user_activity
+from database.db import add_user_activity
 
 logger = logging.getLogger(__name__)
 
@@ -13,26 +15,39 @@ logger = logging.getLogger(__name__)
 class ActivityCounterMiddleware(BaseMiddleware):
     async def __call__(self,
         handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: Update,
+        event: TelegramObject,
         data: dict[str, Any]
     ) -> Any:
+        logger.debug("Входим в ActivityCounterMiddleware")
+
         user: User = data.get("event_from_user")
 
         if user is None:
+            logger.warning("По какой-то неизвестной причине пользователя не удалось определить, переходим в следующий \"обработчик\"")
             return await handler(event, data)
 
         result = await handler(event, data)
 
+        state: FSMContext = data.get("state")
+
+        user_row = await state.get_value("user_row")
+
+        logger.debug("Строка о пользователе: %s", user_row)
+
+        if user_row is None:
+            logger.debug("Данные о пользователе не удалось получить из базы данных, переходим в следующий \"обработчик\"")
+            return result
+
+        logger.debug("Добавляем активность пользователя в БД")
+
         conn: AsyncConnection = data.get("conn")
+
         if conn is None:
             logger.error("Соединение с базой данных не было найдено в данных мидлвари")
             raise RuntimeError("Отстуствует соединение с базой данных для проверки теневого бана")
 
-        row_user = await get_user(conn, user_id=user.id)
-
-        if row_user is None:
-            return result
-
         await add_user_activity(conn, user_id=user.id)
+
+        logger.debug("Выходим из ShadowBanMiddleware")
 
         return result
