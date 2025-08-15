f00f5c22cd55f091eb7e2742b1cca2340d7b0064
dystrophyless <romansokolov689@gmail.com>
2025-08-15 12:00:34 +0500

    Optimize logic so that other middlewares pick up user information from state data, not database, fix wrong logic of start registration and add logs

--- middlewares/registration.py
+++ middlewares/registration.py
@@ -3,7 +3,7 @@ from typing import Any, Awaitable, Callable
 from psycopg import AsyncConnection
 
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Update, User
+from aiogram.types import TelegramObject, User
 from aiogram.fsm.context import FSMContext
 
 from fsm.states import FSMRegister
@@ -12,14 +12,15 @@ from database.db import get_user
 
 logger = logging.getLogger(__name__)
 
-
 class RegistrationMiddleware(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: Update,
+        event: TelegramObject,
         data: dict[str, Any]
     ) -> Any:
+        logger.debug("Входим в RegistrationMiddleware")
+
         user: User = data.get("event_from_user")
 
         if user is None:
@@ -34,19 +35,30 @@ class RegistrationMiddleware(BaseMiddleware):
 
         user_row = await get_user(conn, user_id=user.id)
 
+        logger.debug("Строка о пользователе: %s", user_row)
+
+        state: FSMContext = data.get("state")
+
         if user_row is None:
+            logger.debug("Данные о пользователе не удалось получить из базы данных, определяем роль пользователя, передаём в контекст, устанавливаем состояние регистрации, переходим в следующий \"обработчик\"")
+
             admin_ids: list[int] = data.get("admin_ids")
             if user.id in admin_ids:
                 user_role = UserRole.ADMIN
             else:
                 user_role = UserRole.USER
 
-            state: FSMContext = data.get("state")
-            user_context_data: dict = await state.get_data()
+            logger.debug("Роль пользователя: %s была передана в контекст", user_role)
 
-            user_context_data.update(user_role=user_role)
-            await state.set_data(user_context_data)
-            await state.set_state(FSMRegister.start_register)
+            await state.update_data(user_role=user_role)
 
-        return await handler(event, data)
+            data["start_registration"] = True
+
+        await state.update_data(user_row=user_row)
+
+        result = await handler(event, data)
+
+        logger.debug("Выходим из RegistrationMiddleware")
+
+        return result
 
