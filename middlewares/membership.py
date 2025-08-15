25d18e57a71195a3f81c6b9f8f41b623c0da9e26
dystrophyless <romansokolov689@gmail.com>
2025-08-15 11:57:02 +0500

    Optimize logic so that we don't have to access database multiple times unnecessarily and add logs

--- middlewares/membership.py
+++ middlewares/membership.py
@@ -1,14 +1,12 @@
 import logging
 from typing import Any, Awaitable, Callable
-from psycopg import AsyncConnection
 
 from aiogram import BaseMiddleware
-from aiogram.types import TelegramObject, Update, User
+from aiogram.types import TelegramObject, User
 from aiogram.fsm.context import FSMContext
 
 from fsm.states import FSMMembership
 from services.membership import is_user_followed
-from database.db import get_user
 
 logger = logging.getLogger(__name__)
 
@@ -17,33 +15,37 @@ class MembershipMiddleware(BaseMiddleware):
     async def __call__(
         self,
         handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
-        event: Update,
+        event: TelegramObject,
         data: dict[str, Any],
     ) -> Any:
+        logger.debug("Входим в MembershipMiddleware")
+
         user: User = data.get("event_from_user")
+
+        if user is None:
+            logger.warning("По какой-то неизвестной причине пользователя не удалось определить, переходим в следующий \"обработчик\"")
+            return await handler(event, data)
+
         bot = data.get("bot")
         channel_id = data.get("channel_id")
         state: FSMContext = data.get("state")
 
-        if not user or not state or not bot or not channel_id:
-            return await handler(event, data)
-        
-        conn: AsyncConnection = data.get("conn")
+        user_row = await state.get_value("user_row")
 
-        if conn is None:
-            logger.error("Соединение с базой данных не было найдено в данных мидлвари")
-            raise RuntimeError("Отстуствует соединение с базой данных для проверки теневого бана")
+        logger.debug("Строка о пользователе: %s", user_row)
 
-
-        row_user = await get_user(conn, user_id=user.id)
-
-        if row_user is None:
+        if user_row is None:
+            logger.debug("Данные о пользователе не удалось получить из базы данных, переходим в следующий \"обработчик\"")
             return await handler(event, data)
 
         is_following = await is_user_followed(bot, user.id, channel_id)
 
         if not is_following:
+            logger.debug("Пользователь был не подписан, устанавливаем состояние ожидания подписки, переходим в следующий \"обработчик\"")
             await state.set_state(FSMMembership.await_membership)
-            return
 
-        return await handler(event, data)
+        result = await handler(event, data)
+
+        logger.debug("Выходим из MembershipMiddleware")
+
+        return result
