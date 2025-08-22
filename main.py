56a94a6262430653d747a4c4186bceee0d005c35
dystrophyless <romansokolov689@gmail.com>
2025-08-23 00:15:53 +0500

    change logic due to sqlalchemy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import asyncio
 import logging
 import logging.config
-import psycopg_pool
 import os
 import sys
 import io
@@ -18,7 +17,7 @@ from config_data.config import Config, load_config
 from services.data import load_terms, load_indexed_terms, generate_id_maps
 from handlers import register_handlers, language_handlers, user_handlers, admin_handlers, inline_handlers
 from i18n.translator import get_translations
-from database.connection import get_pg_pool
+from database.connection import get_async_engine, get_sessionmaker
 
 from middlewares.throttler import ThrottlingMiddleware
 from middlewares.database import DatabaseMiddleware
@@ -38,9 +37,9 @@ logger = logging.getLogger(__name__)
 async def main() -> None:
     logging.config.dictConfig(logging_config)
 
-    logger.debug('Процесс запуск бота был начат')
+    logger.debug("Процесс запуск бота был начат")
 
-    config: Config = load_config('.env')
+    config: Config = load_config(".env")
 
     terms = load_terms()
     indexed_terms = load_indexed_terms()
@@ -62,13 +61,15 @@ async def main() -> None:
     )
     dp = Dispatcher(storage=storage)
 
-    db_pool: psycopg_pool.AsyncConnectionPool = await get_pg_pool(
+    engine = get_async_engine(
         db_name=config.db.name,
         host=config.db.host,
         port=config.db.port,
         user=config.db.user,
         password=config.db.password,
     )
+
+    sessionmaker = get_sessionmaker(engine)
 
     translations = get_translations()
     locales = list(translations.keys())
@@ -88,20 +89,20 @@ async def main() -> None:
     dp.update.outer_middleware(LanguageSettingsMiddleware())
     dp.update.outer_middleware(TranslatorMiddleware())
 
-    dp['bot'] = bot
-    dp['channel_id'] = config.bot.channel_id
-    dp['channel_link'] = config.bot.channel_link
-    dp['admin_ids'] = config.bot.admin_ids
-    dp['group_id'] = config.bot.group_id
-    dp['terms'] = terms
-    dp['indexed_terms'] = indexed_terms
-    dp['term_ids'] = term_ids
-    dp['term_names_to_ids'] = term_names_to_ids
-    dp['source_ids'] = source_ids
-    dp['source_names_to_ids'] = source_names_to_ids
-    dp['db_pool'] = db_pool
-    dp['translations'] = translations
-    dp['locales'] = locales
+    dp["bot"] = bot
+    dp["channel_id"] = config.bot.channel_id
+    dp["channel_link"] = config.bot.channel_link
+    dp["admin_ids"] = config.bot.admin_ids
+    dp["group_id"] = config.bot.group_id
+    dp["terms"] = terms
+    dp["indexed_terms"] = indexed_terms
+    dp["term_ids"] = term_ids
+    dp["term_names_to_ids"] = term_names_to_ids
+    dp["source_ids"] = source_ids
+    dp["source_names_to_ids"] = source_names_to_ids
+    dp["sessionmaker"] = sessionmaker
+    dp["translations"] = translations
+    dp["locales"] = locales
 
     try:
         await bot.delete_webhook(drop_pending_updates=True)
@@ -109,10 +110,10 @@ async def main() -> None:
     except Exception as e:
         logger.exception(e)
     finally:
-        await db_pool.close()
+        await engine.dispose()
         logger.debug("Соединение с PostgreSQL было закрыто")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if sys.platform.startswith("win") or os.name == "nt":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
