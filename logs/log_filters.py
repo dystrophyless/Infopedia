d875e336cf114c5a7871c13930524360c16cb1dd
dystrophyless <romansokolov689@gmail.com>
2025-08-23 00:15:26 +0500

    delete unnecessary module

--- logs/log_filters.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-class ErrorLogFilter(logging.Filter):
-    def filter(self, record) -> bool:
-        return record.levelname == 'ERROR'
-
-class CriticalLogFilter(logging.Filter):
-    def filter(self, record) -> bool:
-        return record.levelname == 'CRITICAL'
