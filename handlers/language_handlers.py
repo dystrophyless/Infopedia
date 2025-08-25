aba8fd256d93a2fe2345967c3b5a149f8cb62ef9
dystrophyless <romansokolov689@gmail.com>
2025-08-26 04:01:59 +0500

    migrate from commands to menu buttons

--- handlers/language_handlers.py
+++ handlers/language_handlers.py
@@ -7,6 +7,7 @@ from aiogram.exceptions import TelegramBadRequest
 from aiogram.filters import Command, StateFilter
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery, Message, BotCommandScopeChat
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from filters.filters import LocaleFilter
@@ -46,18 +47,18 @@ async def process_any_message_when_language(
     await state.update_data(language_settings_msg_id=msg.message_id)
 
 
-@router.message(Command(commands=["language"]))
+@router.callback_query(F.data == "change_language")
 async def process_language_command(
-    message: Message,
+    callback: CallbackQuery,
     i18n: dict,
     locales: list[str],
     state: FSMContext,
     session: AsyncSession
 ):
     await state.set_state(FSMLanguage.choose_language)
-    user_language = await get_user_language(session, user_id=message.from_user.id)
+    user_language = await get_user_language(session, user_id=callback.from_user.id)
 
-    msg = await message.answer(
+    msg = await callback.message.answer(
         text=i18n.get("/language"),
         reply_markup=build_language_settings_kb(i18n=i18n, locales=locales, checked=user_language)
     )
@@ -95,11 +96,20 @@ async def process_save_click(
 async def process_cancel_click(
     callback: CallbackQuery,
     i18n: dict,
+    total_users_count: int,
     state: FSMContext,
     session: AsyncSession
 ):
-    user_language = await get_user_language(session, user_id=callback.from_user.id)
-    await callback.message.edit_text(text=i18n.get("language_cancelled").format(i18n.get(user_language)))
+    user_language: str = await get_user_language(session, user_id=callback.from_user.id)
+    user_role: str = await get_user_role(session, user_id=callback.from_user.id)
+
+    await callback.answer(text=i18n.get("language_cancelled").format(i18n.get(user_language)))
+
+    await callback.message.edit_text(
+        text=i18n.get("main_menu").format(total_users_count, i18n.get(user_role)),
+        callback_query="go_to_main_menu"
+    )
+
     await state.update_data(language_settings_msg_id=None, user_language=None)
     await state.set_state()
 
