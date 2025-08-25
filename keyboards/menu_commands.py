a34dce46662223c31100cc4834dae879a28fb8fd
dystrophyless <romansokolov689@gmail.com>
2025-08-26 04:04:45 +0500

    get rid of unnecessary commands due to a migration to a menu buttons

--- keyboards/menu_commands.py
+++ keyboards/menu_commands.py
@@ -12,14 +12,6 @@ def get_main_menu_commands(i18n: dict[str, str], role: UserRole):
                 command="/help",
                 description=i18n.get("/help_description")
             ),
-            BotCommand(
-                command="/find",
-                description=i18n.get("/find_description")
-            ),
-            BotCommand(
-                command="/language",
-                description=i18n.get("/language_description")
-            )
         ]
     elif role == UserRole.ADMIN:
         return [
@@ -30,14 +22,6 @@ def get_main_menu_commands(i18n: dict[str, str], role: UserRole):
             BotCommand(
                 command="/help",
                 description=i18n.get("/help_description")
-            ),
-            BotCommand(
-                command="/find",
-                description=i18n.get("/find_description")
-            ),
-            BotCommand(
-                command="/language",
-                description=i18n.get("/language_description")
             ),
             BotCommand(
                 command="/ban",
