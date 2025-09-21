ba99ea952fff05339a41ebd6b15a605dd5968ed1
dystrophyless <romansokolov689@gmail.com>
2025-09-21 21:22:21 +0500

    change the function according to the change of search function

--- handlers/inline_handlers.py
+++ handlers/inline_handlers.py
@@ -3,6 +3,8 @@ import logging
 from aiogram import Router
 from aiogram.types import InlineQuery, InlineQueryResultArticle
 
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from services.search import search_definitions
 
 logger = logging.getLogger(__name__)
@@ -10,9 +12,9 @@ logger = logging.getLogger(__name__)
 router = Router()
 
 @router.inline_query()
-async def process_search_mode(inline_query: InlineQuery, indexed_terms: list[dict[str, str]]) -> None:
+async def process_search_mode(inline_query: InlineQuery, session: AsyncSession) -> None:
     query: str = inline_query.query.lower()
     user_id: int = inline_query.from_user.id
-    results: list[InlineQueryResultArticle] = search_definitions(query, user_id, indexed_terms)
+    results: list[InlineQueryResultArticle] = await search_definitions(session, query=query, user_id=user_id)
 
     await inline_query.answer(results, cache_time=1)
