75674b387432093eb243ecba8291c54a9a8fd970
dystrophyless <romansokolov689@gmail.com>
2025-09-22 02:19:55 +0500

    get rid of redundant callback factory

--- utils/callback_factories.py
+++ utils/callback_factories.py
@@ -1,9 +1,5 @@
 from aiogram.filters.callback_data import CallbackData
 
-class SourceCallback(CallbackData, prefix='source'):
-    term_id: int
-    source_id: int
-
 
 class TermCallback(CallbackData, prefix='term'):
     term_id: int
