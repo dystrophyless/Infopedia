562c958e74408824293357204bdd203fdbdd9963
dystrophyless <romansokolov689@gmail.com>
2025-12-24 15:17:34 +0500

    add custom exceptions

---
+++ exceptions.py
@@ -0,0 +1,23 @@
+class TermAppError(Exception):
+    pass
+
+
+class TermNotFoundByNameError(TermAppError):
+    def __init__(self, term_name: str):
+        self.term_name = term_name
+        super().__init__(f"Термин `name`='{term_name}' не найден.")
+
+
+class TermNotFoundByIdError(TermAppError):
+    def __init__(self, term_id: int):
+        self.term_id = term_id
+        super().__init__(f"Термин `id`='{term_id}' не найден.")
+
+
+class TermPresentationError(TermAppError):
+    pass
+
+
+class SecurityPayloadError(TermAppError):
+    pass
+
