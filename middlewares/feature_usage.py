78967da0da6c94dfcea6b1eded0a37a6ceedd01a
dystrophyless <romansokolov689@gmail.com>
2026-01-27 01:15:55 +0500

    implement new middleware to log features that user have used

---
+++ middlewares/feature_usage.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Any, Awaitable, Callable
+
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from aiogram.dispatcher.flags import get_flag
+
+from database.db import log_feature_usage
+
+logger = logging.getLogger(__name__)
+
+
+class FeatureUsageMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[TelegramObject, dict[str, Any]], Awaitable[Any]],
+        event: TelegramObject,
+        data: dict[str, Any],
+    ) -> Any:
+        logger.debug("Входим в MenuHistoryMiddleware")
+
+        log_feature_flag = get_flag(data, "log_feature")
+
+        log_feature = log_feature_flag.get("feature") if log_feature_flag else None
+
+        result = await handler(event, data)
+
+        if log_feature:
+            session = data.get("session")
+            user_id = event.from_user.id
+
+            await log_feature_usage(
+                session,
+                user_id=user_id,
+                feature=log_feature
+            )
+
+        logger.debug("Выходим из MenuHistoryMiddleware")
+
+        return result
