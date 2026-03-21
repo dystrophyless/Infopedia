472c1e5e4ea8db934f2257250dad6220c49d68cc
dystrophyless <romansokolov689@gmail.com>
2026-03-21 17:22:43 +0500

    reformat source as book

--- services/term_service.py
+++ services/term_service.py
@@ -2,8 +2,8 @@ import logging
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from database.db import get_source_by_id, get_term_by_id, get_term_by_name
-from database.models import Source, Term
+from database.db import get_book_by_id, get_term_by_id, get_term_by_name
+from database.models import Book, Term
 from exceptions import (
     TermNotFoundByIdError,
     TermNotFoundByNameError,
@@ -36,18 +36,18 @@ class TermService:
         session: AsyncSession,
         *,
         term_id: int,
-        source_id: int,
+        book_id: int,
         index: int = 0,
         i18n: dict,
     ):
         term: Term = await get_term_by_id(session, id=term_id)
-        source: Source = await get_source_by_id(session, id=source_id)
+        book: Book = await get_book_by_id(session, id=book_id)
 
         if term is None:
             logger.debug("Не удалось найти термин `id=%s` в базе данных", term_id)
             raise TermNotFoundByIdError(term_id)
 
-        text, kb = await get_term_info(term=term, source=source, index=index, i18n=i18n)
+        text, kb = await get_term_info(term=term, book=book, index=index, i18n=i18n)
         if text is None:
             logger.debug(
                 "Не удалось перейти к источнику/дефиниции у термина с `name`='%s'",
